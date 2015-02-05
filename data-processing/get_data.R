2a13226cc046fc312b7e5e393e56f5a7f333997b
paldhous <peter@peteraldhous.com>
2015-02-04 22:08:57 -0800

    tweaks to account for changes to API

--- data-processing/get_data.R
+++ data-processing/get_data.R
@@ -6,31 +6,28 @@ library(ggplot2)
 sequence <- seq(0, 500000, by=1000)
 
 # retrieve records at max rate of 1000 at a time, store them in a list of data frames
-baseurl <- "https://localdata-sensors.herokuapp.com/api/sources/[Your-ID]/entries?startIndex="
+url <- "https://localdata-sensors.herokuapp.com/api/sources/[Your-ID]/entries?&count=1000&sort=desc"
 pages <- list()
+
 for(i in sequence){
-  sensor_data <- fromJSON(paste0(baseurl, i, "&count=1000&sort=desc"))
+  sensor_data <- flatten(as.data.frame(fromJSON(url)), recursive = TRUE)
   if(is.na(sensor_data)) break
   message("Retrieving records from: ", i)
+  url <- as.character(sensor_data$links.next[1])
   pages[[i+1]] <- sensor_data
 }
 
 # combine the list into a single data frame
 sensor_data_all <- rbind.pages(pages)
 
-# process data
-sensor_data <- flatten(sensor_data_all, recursive = TRUE) %>%
-  rename(airquality_raw=data.airquality_raw, dust=data.dust, humidity=data.humidity, light=data.light, sound=data.sound, temperature=data.temperature, uv=data.uv) %>%
+# process the data
+sensor_data <- rename(sensor_data_all, timestamp=data.timestamp, airquality_raw=data.data.airquality_raw, dust=data.data.dust, humidity=data.data.humidity, light=data.data.light, sound=data.data.sound, temperature=data.data.temperature, uv=data.data.uv) %>%
   select(timestamp, temperature, humidity, light, sound, airquality_raw, dust, uv) %>%
   mutate(timestamp=as.POSIXct(strptime(timestamp, "%Y-%m-%dT%H:%M:%S.000Z"))) %>%
   filter(timestamp>"2015-01-17 00:00:00")
 
 # write to CSV file
 write.csv(sensor_data, file="[Path-to-file]/sensor_data.csv", row.names=FALSE, quote=FALSE)
-
-
-
-
 
 
 
--- data-processing/update_data.php
+++ data-processing/update_data.php
@@ -2,23 +2,21 @@
 
 $filename="sensor_data.csv";
 
-$content=file_get_contents("https://localdata-sensors.herokuapp.com/api/sources/[Your-ID]/entries?startIndex=0&count=357&sort=desc");
+$content=file_get_contents("https://localdata-sensors.herokuapp.com/api/sources/[Your-ID]/entries?&count=358&sort=desc");
 $data=json_decode($content, TRUE);
 
 $handle=fopen($filename, "a");
 
-$update=array();
-
-foreach (range(0, 356) as $number) {
-	$timestamp=($data[$number]["timestamp"]);
+foreach (range(0, 357) as $number) {
+	$timestamp=($data["data"][$number]["timestamp"]);
 	$timestamp=substr($timestamp, 0, 10) . " " . substr($timestamp, 11, 8);
-	$temperature=($data[$number]["data"]["temperature"]);
-	$humidity=($data[$number]["data"]["humidity"]);
-	$light=($data[$number]["data"]["light"]);
-	$sound=($data[$number]["data"]["sound"]);
-	$airquality=($data[$number]["data"]["airquality_raw"]);
-	$dust=($data[$number]["data"]["dust"]);
-	$uv=($data[$number]["data"]["uv"]);
+	$temperature=($data["data"][$number]["data"]["temperature"]);
+	$humidity=($data["data"][$number]["data"]["humidity"]);
+	$light=($data["data"][$number]["data"]["light"]);
+	$sound=($data["data"][$number]["data"]["sound"]);
+	$airquality=($data["data"][$number]["data"]["airquality_raw"]);
+	$dust=($data["data"][$number]["data"]["dust"]);
+	$uv=($data["data"][$number]["data"]["uv"]);
 	$line=array($timestamp, $temperature, $humidity, $light, $sound, $airquality, $dust, $uv);
     fputcsv($handle, $line);
 }
